b2566e9654abec592407fba660816301ca43f7b7
ElBocetoDelPulgarcito <90299064+ElBocetoDelPulgarcito@users.noreply.github.com>
2021-09-28 20:12:20 -0600

    Add files via upload

--- contacto/correo.php
+++ contacto/correo.php
@@ -11,5 +11,4 @@ $mensajecompleto = $mensaje . "\nAtentamente: " . $nombre;
 mail($destinatario, $asunto, $mensajecompleto, $header);
 echo "<script>alert('correo enviado exitosamente')</script>";
 echo "<script>setTimeout(\"location.href='../index.html'\",1000)</script>";
-
 ?>
