a81b7b30206d597463672bef77f84815c173f00a
ElBocetoDelPulgarcito <90299064+ElBocetoDelPulgarcito@users.noreply.github.com>
2021-09-28 19:39:02 -0600

    Add files via upload

---
+++ contacto/correo.php
@@ -0,0 +1,15 @@
+<?php
+$destinatario = 'elboceto.contactos@gmail.com';
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+
+$header = "Enviado desde la página de El Boceto Del Pulgarcito";
+$mensajecompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+mail($destinatario, $asunto, $mensajecompleto, $header);
+echo "<script>alert('correo enviado exitosamente')</script>";
+echo "<script>setTimeout(\"location.href='../index.html'\",1000)</script>";
+
+?>
